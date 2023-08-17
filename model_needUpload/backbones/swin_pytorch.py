ddb38385e67f5f073f5fae2d2ceb4c451c6e1c44
Yuan Bian <83106338+yuanbianGit@users.noreply.github.com>
2023-08-17 16:59:43 +0800

    Add files via upload

---
+++ model_needUpload/backbones/swin_pytorch.py
@@ -0,0 +1,119 @@
+import torch
+import torch.nn as nn
+from torch.nn import init
+from torchvision import models
+from torch.autograd import Variable
+import timm
+import math
+
+
+def weights_init_kaiming(m):
+    classname = m.__class__.__name__
+    # print(classname)
+    if classname.find('Conv') != -1:
+        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in') # For old pytorch, you may use kaiming_normal.
+    elif classname.find('Linear') != -1:
+        init.kaiming_normal_(m.weight.data, a=0, mode='fan_out')
+    elif classname.find('BatchNorm1d') != -1:
+        init.normal_(m.weight.data, 1.0, 0.02)
+    elif classname.find("BatchNorm2d") != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
+    if hasattr(m, 'bias') and m.bias is not None:
+        init.constant_(m.bias.data, 0.0)
+
+
+def weights_init_classifier(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        nn.init.normal_(m.weight, std=0.001)
+        if m.bias:
+            nn.init.constant_(m.bias, 0.0)
+
+
+# Defines the new fc layer and classification layer
+# |--Linear--|--bn--|--relu--|--Linear--|
+class ClassBlock(nn.Module):
+    def __init__(self, input_dim, class_num, droprate, relu=False, bnorm=True, linear=512, return_f = False):
+        super(ClassBlock, self).__init__()
+        self.return_f = return_f
+        add_block = []
+        if linear>0:
+            add_block += [nn.Linear(input_dim, linear)]
+        else:
+            linear = input_dim
+        if bnorm:
+            add_block += [nn.BatchNorm1d(linear)]
+        if relu:
+            add_block += [nn.LeakyReLU(0.1)]
+        if droprate>0:
+            add_block += [nn.Dropout(p=droprate)]
+        add_block = nn.Sequential(*add_block)
+        add_block.apply(weights_init_kaiming)
+
+        classifier = []
+        classifier += [nn.Linear(linear, class_num)]
+        classifier = nn.Sequential(*classifier)
+        classifier.apply(weights_init_classifier)
+
+        self.add_block = add_block
+        self.classifier = classifier
+    def forward(self, x):
+        x = self.add_block(x)
+        if self.return_f:
+            f = x
+            x = self.classifier(x)
+            return [x,f]
+        else:
+            x = self.classifier(x)
+            return x
+
+
+class swin(nn.Module):
+    def __init__(self, class_num,  camera_num, view_num, cfg, factory, droprate=0.5, stride=2, circle=False, linear_num=512):
+        super(swin, self).__init__()
+
+        model_path = cfg.MODEL.PRETRAIN_PATH
+        model_ft = timm.create_model('swin_base_patch4_window7_224', checkpoint_path=model_path, pretrained=True)
+        # avg pooling to global pooling
+        #model_ft.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        model_ft.head = nn.Sequential() # save memory
+        self.in_planes = 1024
+        self.model = model_ft
+        self.circle = circle
+        self.num_classes = class_num
+        self.ID_LOSS_TYPE = cfg.MODEL.ID_LOSS_TYPE
+        self.neck_feat = cfg.TEST.NECK_FEAT
+
+        self.bottleneck = nn.BatchNorm1d(self.in_planes)
+        self.bottleneck.bias.requires_grad_(False)
+        self.bottleneck.apply(weights_init_kaiming)
+
+
+        self.classifier = nn.Linear(self.in_planes, self.num_classes, bias=False)
+        self.classifier.apply(weights_init_classifier)
+
+    def forward(self, x, label=None, cam_label= None, view_label=None):
+        global_feat = self.model.forward_features(x) # （16,49,1024）-->avragePooling (16,1024)
+        global_feat_bn = self.bottleneck(global_feat)
+
+        if self.training:
+            if self.ID_LOSS_TYPE in ('arcface', 'cosface', 'amsoftmax', 'circle'):
+                cls_score = self.classifier(global_feat_bn, label)
+            else:
+                cls_score = self.classifier(global_feat_bn)
+
+            return cls_score, global_feat  # global feature for triplet loss
+        else:
+            if self.neck_feat == 'after':
+                # print("Test with feature after BN")
+                return global_feat_bn
+            else:
+                # print("Test with feature before BN")
+                return global_feat
+
+    def load_param(self, trained_path):
+        param_dict = torch.load(trained_path)
+        for i in param_dict:
+            self.state_dict()[i.replace('module.', '')].copy_(param_dict[i])
+        print('Loading pretrained model from {}'.format(trained_path))
